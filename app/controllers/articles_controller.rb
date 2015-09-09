c888c1fd27c50af076c22ddfb133ba51b5648875
camihayes <camihayes72@gmail.com>
2015-09-09 15:58:40 +0000

    add validations to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,8 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article has been created!"
       redirect_to articles_path
     else
-      flash[:danger] = "Article has not been created."
+      flash.now[:danger] = "Article has not been created."
+      render :new
     end
   end
 
